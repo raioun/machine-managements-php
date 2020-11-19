e5e0f6844cc56181fd8cd142fd08b45c6592bace
raioun <barubarosuraimu@yahoo.co.jp>
2020-11-19 04:41:04 +0000

    seeder for heroku

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,15 +13,19 @@ class DatabaseSeeder extends Seeder
    // 不要なデータ分のコメントアウトを忘れずに
   public function run()
   {
-    // $this->call(CustomersTableSeeder::class);
-    // $this->call(OrderersTableSeeder::class);
-    // $this->call(ProjectsTableSeeder::class);
-    // $this->call(CompaniesTableSeeder::class);
-    // $this->call(BranchesTableSeeder::class);
-    // $this->call(StoragesTableSeeder::class);
-    // $this->call(MachinesTableSeeder::class);
-    // $this->call(RentalMachinesTableSeeder::class);
+    $this->call(CustomersTableSeeder::class);
+    $this->call(OrderersTableSeeder::class);
+    $this->call(ProjectsTableSeeder::class);
+    $this->call(CompaniesTableSeeder::class);
+    $this->call(BranchesTableSeeder::class);
+    $this->call(StoragesTableSeeder::class);
+    $this->call(MachinesTableSeeder::class);
+    $this->call(RentalMachinesTableSeeder::class);
+    
+    // ↓他のseederと切り離して使用
     // $this->call(OrdersTableSeeder::class);
+    
+    // ↓使用不可
     // $this->call(UsersTableSeeder::class);
   }
 }
